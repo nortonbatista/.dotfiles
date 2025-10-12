6193a15c746d1336e2606ad4cfe02a1d49071954
nortonbatista <49415914+nortonbatista@users.noreply.github.com>
2025-10-12 18:57:12 -0300

    Remove autologin workaround

--- modules/desktop/gnome/default.nix
+++ modules/desktop/gnome/default.nix
@@ -9,10 +9,6 @@
   services.displayManager.autoLogin.enable = true;
   services.displayManager.autoLogin.user = "norton";
 
-  # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
-  systemd.services."getty@tty1".enable = false;
-  systemd.services."autovt@tty1".enable = false;
-
   # Remove NixOS documentation pkg.
   documentation.enable = false;
 
