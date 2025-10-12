0b87baaf3b063cb7c886bb82dbcdbc0eba3e05f9
nortonbatista <49415914+nortonbatista@users.noreply.github.com>
2025-10-11 22:16:52 -0300

    Remove some default pkgs

--- modules/desktop_managers/gnome/default.nix
+++ modules/desktop_managers/gnome/default.nix
@@ -15,4 +15,29 @@
   # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
   systemd.services."getty@tty1".enable = false;
   systemd.services."autovt@tty1".enable = false;
+
+  # Remove NixOS documentation pkg.
+  documentation.enable = false;
+
+  # Remove XTerm pkg.
+  services.xserver.excludePackages = with pkgs; [ xterm ];
+
+  # Remove some gnome default pkgs.
+  services.gnome.core-developer-tools.enable = false;
+  services.gnome.games.enable = false;
+  environment.gnome.excludePackages = with pkgs; [
+    gnome-tour
+    gnome-user-docs
+    gnome-connections
+    gnome-maps
+    gnome-characters
+    gnome-contacts
+    gnome-weather
+    gnome-music
+    snapshot
+    epiphany
+    geary
+    yelp
+    simple-scan
+  ];
 }
