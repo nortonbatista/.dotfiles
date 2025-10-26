dc139b2de0d52f031c67a6f97d91c79ec2bdb4ed
nortonbatista <49415914+nortonbatista@users.noreply.github.com>
2025-10-26 11:30:04 -0300

    Use the nix-minecraft to setup the minecraft server

--- hosts/ghetsis/modules/gamming/minecraft/server.nix
+++ hosts/ghetsis/modules/gamming/minecraft/server.nix
@@ -1,9 +1,24 @@
-{ config, pkgs, ... }:
+{
+  config,
+  pkgs,
+  inputs,
+  ...
+}:
 
 {
-  services.minecraft-server = {
+  imports = [ inputs.nix-minecraft.nixosModules.minecraft-servers ];
+  nixpkgs.overlays = [ inputs.nix-minecraft.overlay ];
+
+  services.minecraft-servers = {
     enable = true;
     eula = true;
     openFirewall = true;
+
+    servers = {
+      me-and-the-boys = {
+        enable = true;
+        package = pkgs.fabricServers.fabric-1_21_10;
+      };
+    };
   };
 }
