6250278331de64cd03027cf95c532d622df37ff1
nortonbatista <49415914+nortonbatista@users.noreply.github.com>
2025-10-28 21:20:20 -0300

    Create minecraft-servers#cobblemon config

--- hosts/ghetsis/modules/gamming/minecraft/server.nix
+++ hosts/ghetsis/modules/gamming/minecraft/server.nix
@@ -15,9 +15,25 @@
     openFirewall = true;
 
     servers = {
-      me-and-the-boys = {
+      cobblemon = {
         enable = true;
-        package = pkgs.fabricServers.fabric-1_21_10;
+        package = pkgs.fabricServers.fabric-1_21_1;
+
+        symlinks = {
+          mods = pkgs.linkFarmFromDrvs "mods" (
+            builtins.attrValues {
+              Fabric = pkgs.fetchurl {
+                url = "https://cdn.modrinth.com/data/P7dR8mSH/versions/m6zu1K31/fabric-api-0.116.7%2B1.21.1.jar";
+                sha512 = "DXv5flFs/bdC1+N6RW7VH5bEbqwGDA8rgDOAiWcLOKui96mDfl4Hpr3L9zLpAgFPsSAvbhjgDW0rVgqE3fnAJA==";
+              };
+              Cobblemon = pkgs.fetchurl {
+                url = "https://cdn.modrinth.com/data/MdwFAVRL/versions/v77SHSXW/Cobblemon-fabric-1.6.1%2B1.21.1.jar";
+                sha512 = "twgr7+4H79PgxYV4B/c5CC31mU7A5/4yF85s2ux9LKR+1RvRKQlv1Oyoy8995BXRRgKGijNXmA/4jlWzFI3H9A==";
+              };
+            }
+          );
+        };
+
       };
     };
   };
