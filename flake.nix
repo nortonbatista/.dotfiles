4369ce66311543a131d4d3742400cef5cf440b6c
nortonbatista <49415914+nortonbatista@users.noreply.github.com>
2025-11-02 13:08:46 -0300

    Add nixpkgs-ruby source

--- flake.nix
+++ flake.nix
@@ -8,6 +8,8 @@
       url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    nixpkgs-ruby.url = "github:NixOS/nixpkgs/01d5c88f6d07caf73b35b94fbf6ac4eb502201d1";
   };
 
   outputs =
@@ -16,14 +18,19 @@
       nixpkgs,
       home-manager,
       nix-minecraft,
+      nixpkgs-ruby,
       ...
     }:
 
+    let
+      system = "x86_64-linux";
+      pkgsRuby = nixpkgs-ruby.legacyPackages.${system};
+    in
     {
       nixosConfigurations = {
         ghetsis = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          specialArgs = { inherit nix-minecraft; };
+          inherit system;
+          specialArgs = { inherit nix-minecraft pkgsRuby; };
           modules = [
             ./hosts/ghetsis
             home-manager.nixosModules.home-manager
