cde5d0098dcc92c6dce255a1a2c5c8c291c4778f
nortonbatista <49415914+nortonbatista@users.noreply.github.com>
2025-11-11 19:18:50 -0300

    Make nix-minecraft follows nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,10 +2,15 @@
   description = "NixOS Flake";
 
   inputs = {
-    nix-minecraft.url = "github:Infinidoge/nix-minecraft";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
     home-manager = {
       url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nix-minecraft = {
+      url = "github:Infinidoge/nix-minecraft";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
